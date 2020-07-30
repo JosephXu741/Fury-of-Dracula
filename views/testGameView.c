f251e049de3ed792b340f8d014a4ecbcb46ca47e
Danny <zhou.danny@hotmail.com>
2020-07-30 20:22:33 +1000

    removed a printf debugging line

--- views/testGameView.c
+++ views/testGameView.c
@@ -251,7 +251,6 @@ int main(void)
 		
 		Message messages[10] = {};
 		GameView gv = GvNew(trail, messages);
-		printf("health %d\n", GvGetHealth(gv, PLAYER_DRACULA));
 		assert(GvGetHealth(gv, PLAYER_DRACULA) ==
 				GAME_START_BLOOD_POINTS + (2 * LIFE_GAIN_CASTLE_DRACULA));
 		assert(GvGetPlayerLocation(gv, PLAYER_DRACULA) == CASTLE_DRACULA);
